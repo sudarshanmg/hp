923daa0f43084981094438576b4ca4d3c55fe033
Sudarshan MG <sudarshanmallibhat@gmail.com>
2022-11-03 19:53:08 +0530

    changed the font and added more styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ function Header() {
   return (
     <div className={classes['header-container']}>
       <header className={classes.header}>Harry Potter</header>
+      <header className={classes.tagline}>Welcome to the Harry Potter Library.</header>
     </div>
   );
 }
